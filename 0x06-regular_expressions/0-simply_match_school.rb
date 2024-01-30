bfd4efe8603a9f6931ee49f740f20e1e0a14ae1a
nimiabaga <nimiabaga@yahoo.com>
2024-01-30 17:07:09 +0100

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,3 @@
 #!/usr/bin/env ruby
-
-# Check if the argument matches the regular expression for "School"
-if ARGV.length == 1
-  input_string = ARGV[0]
-  regex = /School/
-
-  # Use the match method to check if the input string contains the pattern
-  match_result = input_string.match(regex)
-
-  # Output the result
-  if match_result
-    puts "#{match_result[0]}$"
-  else
-    puts "$"
-  end
-else
-  puts "Usage: #{$PROGRAM_NAME} <input_string>"
-end
-
+# Regular expression simply matching school
+puts ARGV[0].scan(/School/).join
