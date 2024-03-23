3309cb15dbfc35dc8f0e12d95570d205b31c0dfd
nimiabaga <nimiabaga@yahoo.com>
2024-03-23 01:14:44 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Using Puppet install flask
+# 1-install_a_package.pp
 
-package {'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# First part of the code to install Flask package
+package { 'flask':
+  ensure => '2.1.0',
 }
+
+# Second part of the code to install Werkzeug package
+package { 'werkzeug':
+  ensure => '2.1.1',
+}
+
